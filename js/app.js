6c7a92ecc8caacfa0ed35ee5179560ec8f376ed9
j-hha <j-hha@bla.bla>
2017-03-25 18:09:29 -0400

    added logic to update status bar dynamically using jQuery

--- js/app.js
+++ js/app.js
@@ -5,16 +5,39 @@ $(function() {
     // the game board section
     $gameBoard: $('#game-board'),
     // the fox img
-    $fox: $('#fox')
+    $fox: $('#fox'),
+    $health: $('#health'),
+    $turn: $('#turn'),
+    $eggs: $('#eggs')
   };
 
-  //
+  // object holding functions relevant to the game logic
   var gameLogic = {
+          // function gets the current position of the element that is passed in as a parameter
     getCurrentPos: function(element) {
-      // function gets the current position of the element that is passed in as a parameter
       return element.offset();
     }
-  }
+  };
+
+  // object holding functions that update elements in the view
+  var viewUpdates = {
+    // updates status bar to reflect current health points
+    updateHealth: function() {
+      for (var i = healthPoints; i>=1; i--) {
+        domElements.$health.append($('<span>').html('&#x2764;'));
+      }
+    },
+    // updates status bar to reflect current # of eggs in barn
+    updateEggs: function() {
+      for (var i = numOfEggs; i>=1; i--) {
+        domElements.$eggs.append($('<span>').html('&#x2b2e;'));
+      }
+    },
+    // updates status bar to reflect current turn #
+    updateTurn: function() {
+      domElements.$turn.append($('<span>').html('night ' + numOfTurn));
+    }
+  };
 
   // object stores event handlers
   var eventHandlers = {
@@ -38,12 +61,21 @@ $(function() {
   //eventListeners
   $(document).keydown(eventHandlers.moveFox);
 
+  // eventually move into start game func
+  viewUpdates.updateHealth();
+  viewUpdates.updateEggs();
+  viewUpdates.updateTurn();
+
+
 });
 
 // global variables
 // initializes position for move functionality
 var pos = 0,
-    posFox = 20;
+    posFox = 20,
+    healthPoints = 10,
+    numOfEggs = 10,
+    numOfTurn = 1;
 
 // movement functions
 var movementFunctionality = {
