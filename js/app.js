e93460c2df16e879f8732ff30bdccf29c3a3c04c
j-hha <j-hha@bla.bla>
2017-03-25 16:30:42 -0400

    added functionality to make fox move and added grass svg

--- js/app.js
+++ js/app.js
@@ -1,7 +1,45 @@
-// console.log('Egg Hunt: app.js connected');
 
 $(function() {
-  // console.log('Egg Hunt: jQuery connected');
+  // object stores DOM elements
+  var domElements = {
+    // the game board section
+    $gameBoard: $('#game-board'),
+    // the fox img
+    $fox: $('#fox')
+  };
 
-  
+  // object stores event handlers
+  var eventHandlers = {
+    // function moves background behind fox from right to left when right arrow is pressed --> fox moves in opposite direction in order to ensure that fox always remains visible --> gives impression that fox moves forward
+    moveFox: function() {
+          console.log('eventHandlers.moveFox is being called');
+      // binds event to right arrow key
+      if (event.which == 38) {
+        event.preventDefault();
+      }
+      // calls function that increases position
+      movementFunctionality.moveForward();
+      // updates position of background in css using value update from moveForward function
+      domElements.$gameBoard.css('right', pos + 'px');
+      // updates position of fox in css using value update from moveForward function
+      domElements.$fox.css('left', posFox + 'px');
+    }
+  };
+
+  //eventListeners
+  $(document).keydown(eventHandlers.moveFox);
 });
+
+// global variables
+// initializes position for move functionality
+var pos = 0,
+    posFox = 20;
+
+// movement functions
+var movementFunctionality = {
+  // function increases global position variable by 10
+  moveForward: function() {
+    pos += 10;
+    posFox += 11;
+  }
+}
